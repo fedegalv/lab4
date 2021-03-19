354f9b239f698ae0e81bbb6a747a26653878edd1
Federico <fedegv.4991@gmail.com>
2021-03-18 21:38:19 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'lab-primero';
+  suma:number = 0;
+  promedio:number = 0;
+  edadUno:number = 0;
+  edadDos:number = 0;
+  Calcular(){
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+  LimpiarInputs(){
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.suma = 0;
+    this.edadDos = 0;
+  }
+}
